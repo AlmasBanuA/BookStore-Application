1046f79a3a4faf7e77ebe66570f0a715a787bd93
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-05-07 11:46:12 +0530

    Book Store Application Backend Programming

--- src/main/java/com/bookstoreapplication/repository/BookStoreRepository.java
+++ src/main/java/com/bookstoreapplication/repository/BookStoreRepository.java
@@ -2,9 +2,20 @@ package com.bookstoreapplication.repository;
 
 import com.bookstoreapplication.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookStoreRepository extends JpaRepository<Book,Integer> {
 
+    @Query(value = "select * from book where book_name like :bookName%", nativeQuery = true)
+    List<Book> findByBookName(String bookName);
+
+    @Query(value = "select * from book order by price", nativeQuery = true)
+    List<Book> getSortedListOfBooksInAsc();
+
+    @Query(value = "select * from book order by price desc", nativeQuery = true)
+    List<Book> getSortedListOfBooksInDesc();
 }
